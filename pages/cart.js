0598e6ba6fb8cbb8cfc0f364861dc3be3f0806bc
Antartic4 <robjose1996@hotmail.com>
2022-10-26 22:43:07 -0400

    jfae

--- pages/cart.js
+++ pages/cart.js
@@ -75,7 +75,7 @@ function CartScreen() {
                     <td className="p-5 text-right">${item.price}</td>
                     <td className="p-5 text-center">
                       <button onClick={() => removeItemHandler(item)}>
-                        <XCircleIcon className="h-5 w-5"></XCircleIcon>
+                        <XCircleIcon className="w-5 h-5"></XCircleIcon>
                       </button>
                     </td>
                   </tr>
@@ -83,7 +83,7 @@ function CartScreen() {
               </tbody>
             </table>
           </div>
-          <div className="card p-5">
+          <div className="p-5 card">
             <ul>
               <li>
                 <div className="pb-3">
@@ -93,8 +93,8 @@ function CartScreen() {
               </li>
               <li>
                 <button
-                  onClick={() => router.push('/shipping')}
-                  className="primary-button w-full"
+                  onClick={() => router.push('login?redirect=/shipping')}
+                  className="w-full primary-button"
                 >
                   Check Out
                 </button>
