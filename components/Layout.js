319d6be452cdaf9ddb7417bbb226ff933223c1e6
Antartic4 <robjose1996@hotmail.com>
2022-10-26 20:47:19 -0400

    Changes

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import React from 'react';
+import Link from 'next/link';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Store' : 'MF - Store'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 px-4 justify-between shadow-md items-center">
+            <Link legacyBehavior href="/">
+              <a className="text-lg font-bold">MF-Store</a>
+            </Link>
+            <div>
+              <Link legacyBehavior href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link legacyBehavior href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          <p>Copyright © 2022 Mariel Frias - Store</p>
+        </footer>
+      </div>
+    </>
+  );
+}
