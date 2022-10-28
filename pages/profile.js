16a2b8f5a74543b015434778b359e11e7f8c0831
Antartic4 <robjose1996@hotmail.com>
2022-10-28 19:17:30 -0400

    wao

---
+++ pages/profile.js
@@ -0,0 +1,137 @@
+import React, { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { getError } from '../utils/error';
+import axios from 'axios';
+import Layout from '../components/Layout';
+
+export default function ProfileScreen() {
+  const { data: session } = useSession();
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    setValue('name', session.user.name);
+    setValue('email', session.user.email);
+  }, [session.user, setValue]);
+
+  const submitHandler = async ({ name, email, password }) => {
+    try {
+      await axios.put('/api/auth/update', {
+        name,
+        email,
+        password,
+      });
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      });
+      toast.success('Perfil actualizado exitosamente.');
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
+  return (
+    <Layout title="Perfil">
+      <form
+        className="max-w-screen-md mx-auto"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Actualizar Perfil</h1>
+        <div className="mb-4">
+          <label htmlFor="name">Nombre</label>
+          <input
+            type="text"
+            className="w-full"
+            id="name"
+            autoFocus
+            {...register('name', {
+              required: 'Porfavor digite nombre',
+            })}
+          />
+          {errors.name && (
+            <div className="text-red-500">{errors.name.message}</div>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="w-full"
+            id="email"
+            {...register('email', {
+              required: 'Introduzca su correo porfavor',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Introduzca un correo valido porfavor',
+              },
+            })}
+          />
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="password">Clave</label>
+          <input
+            className="w-full"
+            type="password"
+            id="password"
+            {...register('password', {
+              minLength: {
+                value: 6,
+                message: 'clave debe contener al menos 5 caracteres.',
+              },
+            })}
+          />
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="confirmPassword">Confirmar Clave</label>
+          <input
+            className="w-full"
+            type="password"
+            id="confirmPassword"
+            {...register('confirmPassword', {
+              validate: (value) => value === getValues('password'),
+              minLength: {
+                value: 6,
+                message: 'clave debe contener al menos 5 caracteres.',
+              },
+            })}
+          />
+          {errors.confirmPassword && (
+            <div className="text-red-500 ">
+              {errors.confirmPassword.message}
+            </div>
+          )}
+          {errors.confirmPassword &&
+            errors.confirmPassword.type === 'validate' && (
+              <div className="text-red-500 ">Las claves no son iguales</div>
+            )}
+        </div>
+        <div className="mb-4">
+          <button className="primary-button">Actualizar Perfil</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
+
+ProfileScreen.auth = true;
