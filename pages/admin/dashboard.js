61288986218ea45d792e925379fe33bc0e8e8ccf
Antartic4 <robjose1996@hotmail.com>
2022-10-29 15:18:00 -0400

    hehe

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,8 +1,81 @@
+import axios from 'axios';
 import Link from 'next/link';
-import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import React, { useEffect, useReducer } from 'react';
 import Layout from '../../components/Layout';
+import { getError } from '../../utils/error';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, summary: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
 
 function AdminDashboardScreen() {
+  const [{ loading, error, summary }, dispatch] = useReducer(reducer, {
+    loading: true,
+    summary: { salesData: [] },
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/summary`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const data = {
+    labels: summary.salesData.map((x) => x._id),
+    datasets: [
+      {
+        label: 'Ventas',
+        backgroundColor: 'rgba(162,222,208,1)',
+        data: summary.salesData.map((x) => x.totalSales),
+      },
+    ],
+  };
+
   return (
     <Layout title="Admin Dashboard">
       <div className="grid md:grid-cols-4 md:gap-5">
@@ -39,21 +112,42 @@ function AdminDashboardScreen() {
           ) : (
             <div>
               <div className="grid grid-cols-1 md:grid-cols-4">
-                <div className="card m-5 p-5">
+                <div className="p-5 m-5 card">
                   <p className="text-3xl">${summary.ordersPrice}</p>
                   <p>Ventas</p>
                   <Link legacyBehavior href="/admin/orders">
                     Ver Ventas
                   </Link>
                 </div>
-                <div className="card m-5 p-5">
-                  <p className="text-3xl">${summary.ordersCount}</p>
+                <div className="p-5 m-5 card">
+                  <p className="text-3xl">{summary.ordersCount}</p>
                   <p>Ordenes</p>
                   <Link legacyBehavior href="/admin/orders">
                     Ver Ordenes
                   </Link>
                 </div>
+                <div className="p-5 m-5 card">
+                  <p className="text-3xl">{summary.productsCount}</p>
+                  <p>Productos</p>
+                  <Link legacyBehavior href="/admin/products">
+                    Ver Productos
+                  </Link>
+                </div>
+                <div className="p-5 m-5 card">
+                  <p className="text-3xl">{summary.usersCount}</p>
+                  <p>Usuarios</p>
+                  <Link legacyBehavior href="/admin/users">
+                    Ver Usuarios
+                  </Link>
+                </div>
               </div>
+              <h2 className="text-xl">Reporte de Ventas</h2>
+              <Bar
+                options={{
+                  legend: { display: true, positions: 'right' },
+                }}
+                data={data}
+              />
             </div>
           )}
         </div>
